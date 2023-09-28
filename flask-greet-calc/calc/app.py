49f004b8cc759e6fbf9e17dab1b75502709c69ed
DoctorMeat <122049725+DoctorMeat@users.noreply.github.com>
2023-09-28 12:08:08 -0400

    Flask 22.1
    adding flask 22.1

---
+++ flask-greet-calc/calc/app.py
@@ -0,0 +1,23 @@
+# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+"""All available operators"""
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div,
+        }
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
